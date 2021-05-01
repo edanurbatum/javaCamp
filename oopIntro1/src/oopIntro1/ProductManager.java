31b4995d17ff50a442d5e2c5868651f14ed20cef
asus <asus@LAPTOP-9IQ5NO3T>
2021-05-02 02:15:50 +0300

    day 2

---
+++ oopIntro1/src/oopIntro1/ProductManager.java
@@ -0,0 +1,11 @@
+package oopIntro1;
+
+import oopIntro1.Product;
+
+public class ProductManager {
+	public void addToCart(Product product) {
+		System.out.println("Sepete eklendi "+product.getName());
+		
+	}
+
+}
